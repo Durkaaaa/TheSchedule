96bb2af0d358b6f022a62bfe9a11fcae8b2ac277
student <gorshunov.oleg2017@yandex.ru>
2023-03-10 12:43:37 +0300

    *

---
+++ TheSchedule/TheSchedule/Serves/ScheduleServes.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheSchedule.Date;
+using TheSchedule.Entities;
+
+namespace TheSchedule.Serves
+{
+    public class ScheduleServes
+    {
+        public List<Schedule> GetScheduleList()
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                return db.Schedules
+                    .Include(d => d.DayOfTheWeek)
+                    .Include(s => s.Subject)
+                    .Include(c => c.Cabinet)
+                    .Include(l => l.LessonPeriod)
+                    .Include(t => t.Teacher)
+                    .ToList();
+            }
+        }
+
+        public List<Schedule> GetDay(Class @class, int dayId)
+        {
+            return GetScheduleList()
+                .Where(t => t.ClassId == @class.Id && t.DayOfTheWeek.Id == dayId)
+                .ToList();
+        }
+
+
+
+
+
+
+
+
+
+
+        public bool CreateSchedule(Schedule schedule)
+        {
+            if (schedule == null)
+                return false;
+
+
+            return true;
+        }
+
+        public bool RemoveSchedule(Schedule schedule)
+        {
+
+
+            return true;
+        }
+    }
+}
